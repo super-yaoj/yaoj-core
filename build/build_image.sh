6ef78b95546792ceb20fca70e04d063d9c7e96e9
sshwy <jy.cat@qq.com>
2022-07-06 15:22:43 +0800

    build: fix cgo

--- build/build_image.sh
+++ build/build_image.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 go generate
-CC="clang -lpthread" go build -o judgeserver -ldflags="-extldflags=-static" ./cmd/judgeserver 
+CC="clang" go build -o judgeserver -ldflags="-extldflags=-static" ./cmd/judgeserver 
 docker build --file build/Dockerfile -t ghcr.io/super-yaoj/judgeserver .
 rm judgeserver
